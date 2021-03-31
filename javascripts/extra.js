fcc524dcf4765a40af8c75899ae5ce6436fbe699
isontheline <44212923+isontheline@users.noreply.github.com>
2021-03-31 20:05:16 +0000

    Deploying to gh-pages from  @ 86878e44cbb1f416e00514f88a71c16629f00034 🚀

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -1,5 +1,6 @@
 if(navigator.userAgent.indexOf('WebSSH') == 0) {
-	var body = document.querySelector('[data-md-color-scheme=slate]');
+    var body = document.querySelector('[data-md-color-scheme=preference]');
+    body.dataset['mdColorScheme'] = 'slate';
 	body.style.setProperty('--md-default-bg-color', 'black');
 	body.style.setProperty('--md-admonition-bg-color', 'var(--md-footer-bg-color)');
 }
