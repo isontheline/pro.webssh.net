aedd3e6394e2af3aa4ae0279275acb9baf53b157
Arnaud MENGUS <isontheline44212923+isontheline@users.noreply.github.com>
2021-04-03 19:54:25 +0000

    Test

--- src/docs/javascripts/extra.js
+++ src/docs/javascripts/extra.js
@@ -1,8 +1,8 @@
-matchMedia("(prefers-color-scheme: dark)").matches&&document.body.setAttribute("data-md-color-scheme","slate");
+//matchMedia("(prefers-color-scheme: dark)").matches&&document.body.setAttribute("data-md-color-scheme","slate");
 
-setTimeout(function() {
-    matchMedia("(prefers-color-scheme: dark)").matches&&document.body.setAttribute("data-md-color-scheme","slate");
-}, 1);
+console.log('test');
+
+document.body.setAttribute("data-md-color-scheme","slate");
 
 if(navigator.userAgent.indexOf('WebSSH') == 0) {
     var body = document.querySelector('[data-md-color-scheme=slate]');
