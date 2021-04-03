27a07ae4f3392423dae3b0cc236cde7621819eb7
isontheline <44212923+isontheline@users.noreply.github.com>
2021-04-03 20:19:31 +0000

    Deploying to gh-pages from  @ 87b087683056426f1f4360e84712f4805cc17cc4 🚀

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -1,10 +1,15 @@
-//matchMedia("(prefers-color-scheme: dark)").matches&&document.body.setAttribute("data-md-color-scheme","slate");
+if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.setAttribute("data-md-color-scheme","slate");
+}
 
-console.log('test');
-
-document.body.setAttribute("data-md-color-scheme","slate");
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    if(e.matches) {
+        document.body.setAttribute("data-md-color-scheme","slate");
+    }
+});
 
 if(navigator.userAgent.indexOf('WebSSH') == 0) {
+    //document.body.setAttribute("data-md-color-scheme","slate");
     var body = document.querySelector('[data-md-color-scheme=slate]');
 	body.style.setProperty('--md-default-bg-color', 'black');
 	body.style.setProperty('--md-admonition-bg-color', 'var(--md-footer-bg-color)');
