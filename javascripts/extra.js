69d6a83e4040d936c415c6ddbddd51f73818b981
isontheline <44212923+isontheline@users.noreply.github.com>
2021-04-18 19:03:10 +0000

    Deploying to gh-pages from  @ 4398e77935d2ff5f41bcb00aeb4f61b30a5893a7 🚀

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -9,8 +9,14 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e =
 });
 
 if(navigator.userAgent.indexOf('WebSSH') == 0) {
-    //document.body.setAttribute("data-md-color-scheme","slate");
     var body = document.querySelector('[data-md-color-scheme=slate]');
-	body.style.setProperty('--md-default-bg-color', 'black');
-	body.style.setProperty('--md-admonition-bg-color', 'var(--md-footer-bg-color)');
+
+    if(navigator.userAgent.indexOf('WebSSH iOS') == 0) {
+        body.style.setProperty('--md-default-bg-color', 'black');
+
+    } else{
+        body.style.setProperty('--md-default-bg-color', '#1F1E1E');
+    }
+
+    body.style.setProperty('--md-admonition-bg-color', 'var(--md-footer-bg-color)');
 }
