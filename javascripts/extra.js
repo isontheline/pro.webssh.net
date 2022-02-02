bfeea9e829a410ecf5d641e7912b26af4c2b9242
isontheline <44212923+isontheline@users.noreply.github.com>
2022-02-02 19:29:28 +0000

    Deploying to gh-pages from  @ d67e6fbf9bd54cb8d9439e6137ef3468564da919 🚀

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -1,26 +1,3 @@
-if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.body.setAttribute("data-md-color-scheme","slate");
-}
-
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-    if(e.matches) {
-        document.body.setAttribute("data-md-color-scheme","slate");
-    }
-});
-
-if(navigator.userAgent.indexOf('WebSSH') == 0) {
-    var body = document.querySelector('[data-md-color-scheme=slate]');
-
-    if(navigator.userAgent.indexOf('WebSSH iOS') == 0) {
-        body.style.setProperty('--md-default-bg-color', 'black');
-
-    } else{
-        body.style.setProperty('--md-default-bg-color', '#1F1E1E');
-    }
-
-    body.style.setProperty('--md-admonition-bg-color', 'var(--md-footer-bg-color)');
-}
-
 // Hyvor Talk ->
 var HYVOR_TALK_WEBSITE = 6135;
 var HYVOR_TALK_CONFIG = {
@@ -38,3 +15,27 @@ script.type = "text/javascript";
 script.src = "https://talk.hyvor.com/web-api/embed.js";
 document.body.appendChild(script);
 // <- Hyvor Talk
+
+if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.setAttribute("data-md-color-scheme","slate");
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    if(e.matches) {
+        document.body.setAttribute("data-md-color-scheme","slate");
+    }
+});
+
+if(navigator.userAgent.indexOf('WebSSH') == 0) {
+    var body = document.querySelector('[data-md-color-scheme=slate]');
+
+    if(body) {
+        if(navigator.userAgent.indexOf('WebSSH iOS') == 0) {
+            body.style.setProperty('--md-default-bg-color', 'black');
+        } else{
+            body.style.setProperty('--md-default-bg-color', '#1F1E1E');
+        }
+    
+        body.style.setProperty('--md-admonition-bg-color', 'var(--md-footer-bg-color)');
+    }
+}
