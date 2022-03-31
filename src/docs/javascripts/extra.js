9b411a7d38d22ec85c3a1394dabca03014c0c683
Arnaud Mengus <44212923+isontheline@users.noreply.github.com>
2022-03-31 20:14:04 +0000

    💄 Ability to change the primary color on the fly

--- src/docs/javascripts/extra.js
+++ src/docs/javascripts/extra.js
@@ -16,26 +16,34 @@ script.src = "https://talk.hyvor.com/web-api/embed.js";
 document.body.appendChild(script);
 // <- Hyvor Talk
 
-if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.body.setAttribute("data-md-color-scheme","slate");
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.setAttribute("data-md-color-scheme", "slate");
 }
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-    if(e.matches) {
-        document.body.setAttribute("data-md-color-scheme","slate");
+    if (e.matches) {
+        document.body.setAttribute("data-md-color-scheme", "slate");
     }
 });
 
-if(navigator.userAgent.indexOf('WebSSH') == 0) {
+if (navigator.userAgent.indexOf('WebSSH') == 0) {
     var body = document.querySelector('[data-md-color-scheme=slate]');
 
-    if(body) {
-        if(navigator.userAgent.indexOf('WebSSH iOS') == 0) {
+    if (body) {
+        if (navigator.userAgent.indexOf('WebSSH iOS') == 0) {
             body.style.setProperty('--md-default-bg-color', 'black');
-        } else{
+        } else {
             body.style.setProperty('--md-default-bg-color', '#1F1E1E');
         }
-    
+
         body.style.setProperty('--md-admonition-bg-color', 'var(--md-footer-bg-color)');
     }
+}
+
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+
+if (params.primaryColor) {
+    document.documentElement.style.setProperty('--md-primary-fg-color', params.primaryColor, 'important');
 }
