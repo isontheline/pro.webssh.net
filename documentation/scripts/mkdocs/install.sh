8c91b45024bf05fb9e0a5c6b60ba139fbd473818
Arnaud Mengus <44212923+isontheline@users.noreply.github.com>
2023-01-12 22:21:44 +0100

    Fixing https://github.com/squidfunk/mkdocs-material/issues/4843

--- documentation/scripts/mkdocs/install.sh
+++ documentation/scripts/mkdocs/install.sh
@@ -1,5 +1,5 @@
 pip3 install mkdocs
-pip3 install mkdocs-material=="9.*" 
+pip3 install mkdocs-material=="9.0.4" 
 pip3 install mkdocs-git-revision-date-localized-plugin
 pip3 install mkdocs-redirects
 
