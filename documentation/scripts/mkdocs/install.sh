c6617758d47b3a4665a39f10c64952e60f075f02
Arnaud Mengus <44212923+isontheline@users.noreply.github.com>
2023-09-13 22:01:16 +0200

    mkdocs 1.5 doesn't seem to play well with **BOLD**

--- documentation/scripts/mkdocs/install.sh
+++ documentation/scripts/mkdocs/install.sh
@@ -1,6 +1,7 @@
-pip3 install mkdocs
-pip3 install mkdocs-material=="9.0.4" 
-pip3 install mkdocs-git-revision-date-localized-plugin
-pip3 install mkdocs-redirects
+# La version 1.5.x de mkdocs ne fonctionne pas bien avec le bold **TEST**
+pip3.9 install mkdocs=="1.4.3"
+pip3.9 install mkdocs-material=="9.0.4"
+pip3.9 install mkdocs-git-revision-date-localized-plugin
+pip3.9 install mkdocs-redirects
 
 yarn add --dev all-contributors-cli
