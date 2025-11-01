94530dbb7c9a276e83da87078c409fcb232a57ff
isontheline <44212923+isontheline@users.noreply.github.com>
2025-11-01 22:05:51 +0000

    Deploying to gh-pages from  @ a5eae2c90099be2d7f6d67206eb54bb82d051777 🚀

---
+++ javascripts/autorag.js
@@ -0,0 +1,34 @@
+function injectModule(url) {
+    return import(url)
+        .then(function (module) {
+            return module;
+        })
+        .catch(function (error) {
+            console.error('Failed to load module from ' + url + ':', error);
+            throw error;
+        });
+}
+
+injectModule('https://cdn.jsdelivr.net/npm/@n8n/chat/dist/chat.bundle.es.js')
+    .then(function (module) {
+        module.createChat({
+            webhookUrl: 'https://api.mengus.net/webhook/c468d733-6667-42b9-92c2-51ff1e396d88/chat',
+            initialMessages: [
+                "Hey 👋 Ask me anything about WebSSH and I will do my best to help you!",
+            ],
+            enableStreaming: true,
+            i18n: {
+                en: {
+                    title: 'WebSSH Documentation Assistant',
+                    subtitle: "",
+                    footer: '',
+                    getStarted: 'New Conversation',
+                    inputPlaceholder: 'Type your question...',
+                },
+            },
+        });
+    })
+    .catch(function (error) {
+        console.error('Error:', error);
+    });
+
